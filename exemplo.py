181219928fba0e672e032d05b46269147ad2f393
Lara Goncalves <laragonsilva26122003@gmail.com>
2024-01-22 21:14:14 -0300

    sadas

---
+++ exemplo.py
@@ -0,0 +1,6 @@
+'''
+    M: A letra M representa o estado Modified, do português modificado. Isso significa que o arquivo já existia no repositório, mas que recebeu alguma modificação que ainda não foi registrada no Git.
+
+    U: A letra U representa o estado Untracked, do português não rastreado. Isso significa que o arquivo ainda não existia no repositório e que ainda não teve seu registro (commit) feito no Git.
+    
+'''
